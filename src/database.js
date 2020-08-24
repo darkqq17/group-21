d9c2c7b4f0753b354c04572b4062d21cccd92f8b
darkqq17 <b0983729632@gmail.com>
2020-08-24 20:52:10 +0800

    comment

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
-
-module.exports = new Sequelize({
+/*const sequelize = require('../../Downloads/group-21-git-checkout 2/src/database');*/
+const sequelize = new Sequelize({
     host: '140.115.87.119',
     port: 3306,
     database: 'course_system',
@@ -11,3 +11,5 @@ module.exports = new Sequelize({
         timestamps: false
     }
 });
+
+module.exports = sequelize;
