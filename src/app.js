9b3b018dd712e592eba16ad318d4bcb38ba24cb7
DavisLin <davislin@trevi.cc>
2020-09-03 01:03:37 +0800

    update port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const koaBody = require('koa-bodyparser');
 const apiRouter = require('./router/router');
 
 const app = new koa();
-const SERVER_PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 // request -> node.js tcp socket(http) -> koa middlewares
 app.use(logger());
@@ -13,6 +13,6 @@ app.use(koaBody());
 app.use(apiRouter.routes());
 
 // Open a server instance
-app.listen(SERVER_PORT, () => {
-    console.log(`[SERVER] is listening on port ${SERVER_PORT}`)
+app.listen(PORT, () => {
+    console.log(`[SERVER] is listening on port ${PORT}`)
     })
