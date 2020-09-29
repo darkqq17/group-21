f29db26f017ee6e3de3a3b5c03d2b7a2d2db963f
darkqq17 <b0983729632@gmail.com>
2020-09-29 15:21:04 +0800

    init

--- src/app.js
+++ src/app.js
@@ -23,12 +23,3 @@ app.use(cors()); // 跨域
 app.listen(PORT, () => {
     console.log(`[SERVER] is listening on port ${PORT}`)
 })
-
-const cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(PORT, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + PORT);
-});
